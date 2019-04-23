ab8a994d274445ae823fa7b127c9d1d3170b9c23
Aude NOYERE <a.noyere@gmail.com>
2019-04-23 15:02:26 +0200

    ajout de css

--- index.php
+++ index.php
@@ -5,6 +5,20 @@ $resultat = getAllAnnonces();
 
 ?>
 
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        li{
+            list-style: none;
+        }
+    </style>
+</head>
+<body>
 <ul>
  	<li><?php echo $resultat["annonce1"][0] ?></li>
 </ul>
@@ -14,4 +28,14 @@ $resultat = getAllAnnonces();
  	<li><?php echo $resultat["annonce2"][0] ?></li>
  	<li><?php echo $resultat["annonce2"][1] ?></li>
 </ul>
+
+<?php 
+
+$first = getFirstAnnonce();
+print_r($first);
+
+?>
+
+</body>
+</html>
 
