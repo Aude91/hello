9d3012d34308e4c16d6b0f1d238dadab5e9a5d46
Aude NOYERE <a.noyere@gmail.com>
2019-04-23 12:13:19 +0200

    modification affichage

--- index.php
+++ index.php
@@ -7,8 +7,8 @@ $resultat = getAllAnnonces();
 
 <ul>
  	<li><?php echo $resultat["annonce1"][0] ?></li>
- 	<li><?php echo $resultat["annonce1"][1] ?></li>
 </ul>
+<p><?php echo $resultat["annonce1"][0] ?> vaut <?php echo $resultat["annonce1"][1] ?></p>
 <hr>
 <ul>
  	<li><?php echo $resultat["annonce2"][0] ?></li>
